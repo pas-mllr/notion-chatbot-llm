457a06deb787a05c2f67bdb8ba04f7dfa0896b11
ngkhtien <khtien0107@gmail.com>
2023-12-28 16:42:10 +0700

    aaa

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ import time
 import streamlit as st
 from utils import load_chain
 
-# Custom image for the app icon and the assistant's avatar
-company_logo = 'https://blendle.com/img/packaging/android-touch-icon-196x196.png'
 
 # Configure streamlit page
 st.set_page_config(
     page_title="Your Notion Chatbot",
-    page_icon=company_logo
 )
 
 # Initialize LLM chain in session_state
@@ -25,7 +22,7 @@ if 'messages' not in st.session_state:
 # Custom avatar for the assistant, default avatar for user
 for message in st.session_state.messages:
     if message["role"] == 'assistant':
-        with st.chat_message(message["role"], avatar=company_logo):
+        with st.chat_message(message["role"]):
             st.markdown(message["content"])
     else:
         with st.chat_message(message["role"]):
@@ -39,7 +36,7 @@ if query := st.chat_input("Ask me anything"):
     with st.chat_message("user"):
         st.markdown(query)
 
-    with st.chat_message("assistant", avatar=company_logo):
+    with st.chat_message("assistant"):
         message_placeholder = st.empty()
         # Send user's question to our chain
         result = st.session_state['chain']({"question": query})
