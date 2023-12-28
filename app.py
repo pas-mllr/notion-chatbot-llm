79dc676af26b043eb8e5271bfa729970d9593d36
ngkhtien <khtien0107@gmail.com>
2023-12-28 17:00:28 +0700

    aaa

--- app.py
+++ app.py
@@ -20,36 +20,36 @@ if 'messages' not in st.session_state:
 
 # Display chat messages from history on app rerun
 # Custom avatar for the assistant, default avatar for user
-# for message in st.session_state.messages:
-#     if message["role"] == 'assistant':
-#         with st.chat_message(message["role"]):
-#             st.markdown(message["content"])
-#     else:
-#         with st.chat_message(message["role"]):
-#             st.markdown(message["content"])
+for message in st.session_state.messages:
+    if message["role"] == 'assistant':
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 # Chat logic
-# if query := st.chat_input("Ask me anything"):
-#     # Add user message to chat history
-#     st.session_state.messages.append({"role": "user", "content": query})
-#     # Display user message in chat message container
-#     with st.chat_message("user"):
-#         st.markdown(query)
-#
-#     with st.chat_message("assistant"):
-#         message_placeholder = st.empty()
-#         # Send user's question to our chain
-#         result = st.session_state['chain']({"question": query})
-#         response = result['answer']
-#         full_response = ""
-#
-#         # Simulate stream of response with milliseconds delay
-#         for chunk in response.split():
-#             full_response += chunk + " "
-#             time.sleep(0.05)
-#             # Add a blinking cursor to simulate typing
-#             message_placeholder.markdown(full_response + "▌")
-#         message_placeholder.markdown(full_response)
-#
-#     # Add assistant message to chat history
-#     st.session_state.messages.append({"role": "assistant", "content": response})
+if query := st.chat_input("Ask me anything"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": query})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(query)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        # Send user's question to our chain
+        result = st.session_state['chain']({"question": query})
+        response = result['answer']
+        full_response = ""
+
+        # Simulate stream of response with milliseconds delay
+        for chunk in response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+
+    # Add assistant message to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
