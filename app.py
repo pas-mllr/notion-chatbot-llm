b87ef07b8dec39af29c7b21ada3ddec358d19038
ngkhtien <khtien0107@gmail.com>
2023-12-29 16:35:24 +0700

    aaa

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ if query:
         result = chain({"question": query})
         # result = "Sorry. I'm still learning"
         # result = st.session_state['chain']({"question": query})
-        # response = result
+
         full_response = ""
         # Simulate stream of response with milliseconds delay
         for chunk in result.split():
