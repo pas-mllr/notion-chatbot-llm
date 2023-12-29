8ca9a023850f30dbaa22495a410057149aa15035
ngkhtien <khtien0107@gmail.com>
2023-12-29 15:49:41 +0700

    aaa

--- ingest.py
+++ ingest.py
@@ -1,3 +1,5 @@
+#ingest.py
+
 import streamlit as st
 import openai
 from langchain.document_loaders import NotionDirectoryLoader
@@ -8,13 +10,13 @@ from langchain.vectorstores import FAISS
 # Load OpenAI API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Load the notion content located in the notion_content folder
+# Load the Notion content located in the folder 'notion_content'
 loader = NotionDirectoryLoader("notion_content")
 documents = loader.load()
 
-# Split Notion content into smaller chunks
+# Split the Notion content into smaller chunks
 markdown_splitter = RecursiveCharacterTextSplitter(
-    separators=["#","##", "###", "\n\n","\n","."],
+    separators=["#","##", "###", "\\n\\n","\\n","."],
     chunk_size=1500,
     chunk_overlap=100)
 docs = markdown_splitter.split_documents(documents)
@@ -23,7 +25,7 @@ docs = markdown_splitter.split_documents(documents)
 embeddings = OpenAIEmbeddings()
 
 # Convert all chunks into vectors embeddings using OpenAI embedding model
-# Store all vectors in FAISS index and save locally to 'faiss_index'
+# Store all vectors in FAISS index and save to local folder 'faiss_index'
 db = FAISS.from_documents(docs, embeddings)
 db.save_local("faiss_index")
 
